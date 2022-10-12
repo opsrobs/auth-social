e320510728af0723a140445c5a75bc9aa7197a9c
robson.flavio20@outlook.com <opsrobs>
2022-10-12 00:58:00 -0300

    oauth2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,101 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2 v-if="user">User logged: {{ user }}</h2>
+
+    <br />
+    <div id="logout" v-if="isSignedIn">
+      <button @click="handleSignOut">Logout</button>
+    </div>
+
+    <br />
+    <div id="LogingWithGoogle" v-if="!isSignedIn">
+      <h3>Google</h3>
+      <button @click="handleLogingGoogle">Login</button>
+    </div>
+
+    <br />
+    <div id="LoginWithTwitter" v-if="!isSignedIn">
+      <h3>Twitter</h3>
+      <button @click="handleLogingTwitter">Login</button>
+    </div>
+
+    <br />
+    <div id="LoginWithGitHub" v-if="!isSignedIn">
+      <h3>GitHub</h3>
+      <button @click="handleLogingGitHub">Login</button>
+    </div>
   </div>
 </template>
 
 <script>
+import firebaseConfig from '../firebaseConfig';
+import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, TwitterAuthProvider, GithubAuthProvider  } from "firebase/auth";
+
+firebaseConfig
+
+const provider = new GoogleAuthProvider();
+const providerTwitter = new TwitterAuthProvider();
+const providerGithub = new GithubAuthProvider();
+const auth = getAuth();
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      user: '',
+      isSignedIn: false,                    // time 12:00
+    }
+  },
+  methods: {
+    handleLogingGoogle() {
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          //const user = result.user;
+          console.log(result)
+
+          this.user = result.user.displayName;
+          this.isSignedIn = true
+        }).catch((error) => {
+          console.log(error)
+        });
+    },
+    handleSignOut() {
+      const auth = getAuth();
+      signOut(auth).then(() => {
+        this.user = ''
+        this.isSignedIn = false
+      }).catch((error) => {
+        console.log(error)
+      });
+
+    },
+    handleLogingTwitter() {
+      signInWithPopup(auth, providerTwitter)
+      .then((result) => {
+          //const user = result.user;
+          console.log(result)
+
+          this.user = result.user.displayName;
+          this.isSignedIn = true
+        }).catch((error) => {
+          console.log(error)
+        });
+    },
+    handleLogingGitHub(){
+      signInWithPopup(auth, providerGithub)
+      .then((result) => {
+          //const user = result.user;
+          console.log(result)
+
+          this.user = result.user.displayName;
+          this.isSignedIn = true
+        }).catch((error) => {
+          console.log(error)
+        });
+    }
   }
 }
 </script>
@@ -44,14 +105,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
